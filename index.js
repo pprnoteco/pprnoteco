42e3179540444fda8a70b3b4020c3ebd80a7ca05
revivalbodywork <trumbull.j@gmail.com>
2017-05-31 16:20:42 -0400

    added slack/lvl10 response

--- index.js
+++ index.js
@@ -3,10 +3,19 @@ var app = express();
 
 var port = process.env.PORT || 8080;
 
+app.set('view engine', 'pug');
+
 app.get('/', function (req, res) {
   res.send('Hello World!')
 });
 
+app.get('/slack/lvl10', function (req, res) {
+  res.send({
+      response_type: 'in_channel',
+      text: 'Hello world'
+  })
+});
+
 app.listen(port, function () {
-  console.log('Example app listening on port 80')
+  console.log('Example app listening on port ' + port);
 });
