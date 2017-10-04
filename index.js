c5af012fbcf0312752daaddfbc7439c98a527782
Joe Trumbull <trumbull.j@gmail.com>
2017-10-04 13:46:15 -0400

    Test

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
+var bodyParser = require('body-parser');
 var express = require('express');
 var app = express();
 
 var port = process.env.PORT || 8080;
 
-app.set('view engine', 'pug');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/test', function (req, res) {
     res.send({
         response_type: 'in_channel',
-        text: 'Hello world'
+        text: JSON.stringify(req.body)
     });
 });
 
